487481318ba37ac8045cc0c77519e72ff4d4ceaf
Sasha Guo <sashaguo37@gmail.com>
2023-01-31 23:38:42 -0800

    add keyboard and click handlers for letters, add removeGuessedLetter and addGuessedLetter functions, get started on addGuessedWord functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,40 @@ function App() {
 	//loop through random-words until we get 5 letter word
 	let fiveLetterWord: string = "";
 	while (fiveLetterWord.length < 5) {
-		fiveLetterWord = randomWords({ exactly: 1, maxLength: 5 })[0];
+		fiveLetterWord = randomWords({ exactly: 1, maxLength: 5 })[0].toUpperCase();
 	}
 	const [wordtoGuess, setWordToGuess] = useState(fiveLetterWord);
-	const [guessedLetters, setGuessedLetters]: string[] = useState([]);
+	const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+	const [guessedWords, setGuessedWords] = useState<string[]>([]);
+
+	function addGuessedLetter(letter: string) {
+		return setGuessedLetters((prev) => {
+			const updated = [...prev, letter];
+			console.log(updated);
+			return updated;
+		});
+	}
+
+	function removeGuessedLetter() {
+		return setGuessedLetters((prev) => prev.slice(0, prev.length - 1));
+	}
+
+	function addGuessedWord(word: string) {
+		return setGuessedWords((prev) => [...prev, word]);
+	}
 
 	return (
 		<div>
 			{wordtoGuess}
-
+			{guessedLetters}
 			<GuessedWords />
-			<Keyboard />
+			<Keyboard
+				guessedLetters={guessedLetters}
+				addGuessedLetter={addGuessedLetter}
+				guessedWords={guessedWords}
+				addGuessedWord={addGuessedWord}
+				removeGuessedLetter={removeGuessedLetter}
+			/>
 		</div>
 	);
 }
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -27,31 +27,78 @@ const alphabet = [
 	"X",
 	"Y",
 	"Z",
+	"Backspace",
+	"Enter",
 ];
 
 type KeyboardProps = {
 	guessedLetters: string[];
+	addGuessedLetter: Function;
+	guessedWords: string[];
+	addGuessedWord: Function;
+	removeGuessedLetter: Function;
 };
 
-function Keyboard(props: KeyboardProps) {
+function Keyboard({
+	addGuessedLetter,
+	guessedLetters,
+	guessedWords,
+	addGuessedWord,
+	removeGuessedLetter,
+}: KeyboardProps) {
 	useEffect(() => {
-		const handleKeyPress = (e: KeyboardEvent) => {
-			setGuessedLetters();
+		const handleKeydown = (e: KeyboardEvent) => {
+			if (e.key == "Enter") {
+				if (guessedLetters.length == 5) {
+					addGuessedWord(guessedLetters);
+				}
+			}
+			if (e.key == "Backspace" && guessedLetters.length) {
+				removeGuessedLetter();
+				console.log(guessedLetters);
+			}
+			const letterRegex = /^[a-z]$/;
+			if (letterRegex.test(e.key)) {
+				if (guessedLetters.length > 5) {
+					e.preventDefault();
+					return;
+				}
+				addGuessedLetter(e.key.toUpperCase());
+			}
 		};
 
-		document.addEventListener("keypress", handleKeyPress);
+		document.addEventListener("keydown", handleKeydown);
 
 		return () => {
-			document.removeEventListener("keypress", handleKeyPress);
+			document.removeEventListener("keydown", handleKeydown);
 		};
-	}, []);
+	}, [guessedLetters]);
 
 	return (
 		<div>
 			{alphabet.map((letter) => {
-				return <button key={letter}>{letter}</button>;
+				function handleClick() {
+					if (letter == "Backspace" && guessedLetters.length) {
+						removeGuessedLetter();
+					}
+					if (letter == "Enter") {
+						if (guessedLetters.length == 5) {
+							addGuessedWord(guessedLetters);
+						}
+					}
+					if (guessedLetters.length > 5) {
+						return;
+					}
+					addGuessedLetter(letter);
+					console.log(guessedLetters);
+				}
+				return (
+					<button onClick={handleClick} key={letter}>
+						{letter}
+					</button>
+				);
 			})}
-			{props.guessedLetters}
+			{guessedLetters}
 		</div>
 	);
 }
